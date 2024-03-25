4caaf4bedac19e2dc5259dd1ea12e8dc8489bcc8
Andrey <you@example.com>
2024-03-25 20:41:43 +0200

    dgsgf

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
 urlpatterns += static(settings.MEDIA_URL,
 document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
 
